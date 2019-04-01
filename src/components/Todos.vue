b1040e75a56f121cc3e9a50edbf4e1c06aa7eb6f
Nathan Downer <nathandowner123@gmail.com>
2019-04-01 01:08:38 -0500

    addedtodo and todos component

---
+++ src/components/Todos.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div v-bind:key="todo.id" v-for="todo in todos">
+      <Todo v-bind:todo="todo" />
+    </div>
+  </div>
+</template>
+
+<script>
+import Todo from './Todo'
+export default {
+  name: "Todos",
+  components: {
+    Todo
+  },
+  props: ["todos"]
+}
+</script>
+
+
+<style scoped>
+
+</style>
+
+
